615940dae19f8465a86c685fb79dcfa4f18913cd
Sandeep Sarode <sandeepsarode21@gmail.com>
2021-10-17 08:41:56 -0400

    updated Post and Posts components

---
+++ components/Post.js
@@ -0,0 +1,11 @@
+function Post({ id, img, username, caption }) {
+  return (
+    <div>
+      <h1>i am post</h1>
+      {/* headre */}
+      {/* img */}
+    </div>
+  );
+}
+
+export default Post;
