8e20dfc9358cfb5ba76372c67dcf3dde0ab0a57f
Sandeep Sarode <sandeepsarode21@gmail.com>
2021-10-17 08:20:38 -0400

    Added Story and Post component

--- components/Feed.js
+++ components/Feed.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Post from "./Post.js";
 import Stories from "./Stories.js";
 
 function Feed() {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto">
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
       {/* Section */}
       <section className="col-span-2">
         {/* Stories */}
         <Stories />
         {/* post */}
+        <Post />
       </section>
 
       {/* Section */}
--- components/Stories.js
+++ components/Stories.js
@@ -1,16 +1,27 @@
 import faker from "faker";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Story from "./Story";
 
 function Stories() {
+  const [suggestions, setSuggestions] = useState([]);
+
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
       ...faker.helpers.contextualCard(),
       id: i,
     }));
-    console.log(suggestions);
+    setSuggestions(suggestions);
+    //console.log(suggestions);
   }, []);
   return (
-    <div>
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thumb-black scrollbar-thin">
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
       {/* Story */}
       {/* Story */}
       {/* Story */}
