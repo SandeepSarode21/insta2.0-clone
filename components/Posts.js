0ba61f3fc14d7911461bf873622c1ccfe30b48d4
Sandeep Sarode <sandeepsarode21@gmail.com>
2021-10-17 08:42:17 -0400

    updated posts

--- components/Posts.js
+++ components/Posts.js
@@ -1,5 +1,56 @@
+import Post from "./Post";
+
+const posts = [
+  {
+    id: "123",
+    username: "sarode",
+    userImg:
+      "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png",
+    img: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png",
+    caption: "Lets build it ",
+  },
+  {
+    id: "123",
+    username: "sarode",
+    userImg:
+      "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png",
+    img: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png",
+    caption: "Lets build it ",
+  },
+  {
+    id: "123",
+    username: "sarode",
+    userImg:
+      "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png",
+    img: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png",
+    caption: "Lets build it ",
+  },
+  {
+    id: "123",
+    username: "sarode",
+    userImg:
+      "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png",
+    img: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png",
+    caption: "Lets build it ",
+  },
+];
+
 function Posts() {
-  return <div></div>;
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
+      {/* Post */}
+    </div>
+  );
 }
 
 export default Posts;
